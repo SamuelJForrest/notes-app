88b477bdea49447d76a753805e4d1b240579a2e4
SamuelJForrest <samueljamesforrestweb@gmail.com>
2021-05-03 10:08:15 +0100

    Add files via upload
    Added more styling and some JS functionality - still in the mobile stages of development, so monitor view isn't aesthetically pleasing yet.

--- app.js
+++ app.js
@@ -1,1 +1,81 @@
-console.log('test');
+// define variables: Title, textarea, select, and buttons
+const noteTitleInput = document.querySelector('#note-title-input');
+const noteTextarea = document.querySelector('#note-text');
+const noteSelect = document.querySelector('#note-tag');
+const submitBtn = document.querySelector('#submit-btn');
+const clearBtn = document.querySelector('#clear-btn');
+const notesFeed = document.querySelector('.notes');
+const noteForm = document.querySelector('#note-form');
+const noteInputArea = document.querySelector('.note-input');
+
+// Event listener when form submits - creates note and fills it with info from inputs.
+
+noteForm.addEventListener('submit', (e) => {
+    //Prevent form submitting + create div to contain notes
+    if (noteTitleInput.value === '' || noteTextarea.value === ''){
+        e.preventDefault();
+        let warning = document.createElement('div');
+        warning.classList.add('warning');
+        warning.textContent = 'Please enter valid text.';
+        noteInputArea.append(warning);
+    } else if (noteSelect.value === 'default') {
+        e.preventDefault();
+        let selectWarning = document.createElement('div');
+        selectWarning.classList.add('warning');
+        selectWarning.textContent = 'Please select a priority';
+        noteInputArea.append(selectWarning);
+    } else {
+        e.preventDefault();
+    let note = document.createElement('div');
+    note.classList.add('note');
+    notesFeed.append(note);
+    // Create div to contain note title and text
+    let noteInfo = document.createElement('div');
+    noteInfo.classList.add('note-info');
+    let noteTitle = document.createElement('h2');
+    noteTitle.textContent = noteTitleInput.value;
+    noteInfo.append(noteTitle);
+    let noteText = document.createElement('p');
+    noteText.textContent = noteTextarea.value;
+    noteInfo.append(noteTitle);
+    noteInfo.append(noteText);
+    note.append(noteInfo);
+    // Create delete button image
+    let deleteNote = document.createElement('button');
+    deleteNote.classList.add('delete-btn');
+    deleteNote.textContent = 'X';
+    deleteNote.addEventListener('click', () => {
+        note.remove();
+    })
+    note.append(deleteNote);
+
+    // Use select values to determine the background-color of note
+    if (noteSelect.value === 'low') {
+        note.classList.add('green');
+    } else if (noteSelect.value === 'mid') {
+        note.classList.add('amber');
+    } else if (noteSelect.value === 'high') {
+        note.classList.add('red');
+    }
+    }
+    noteTitleInput.value = '';
+    noteTextarea.value = '';
+    noteSelect.value = 'default';
+});
+
+// Event listener for clear button - removes all text content from inputs and resets select
+
+clearBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    noteTitleInput.value = "";
+    noteTextarea.value = "";
+    noteSelect.value = 'default';
+});
+
+// Event listener for delete button on notes
+function removeNote(e){
+    let deleteBtn = e.target;
+    if (deleteBtn.classList[0] === 'note'){
+        console.log('click');
+    }
+}
